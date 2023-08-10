1a3a5fe9dc35d3d7afc598a02070210c1bb3deaa
Kriukovchankiu <kryukovchanskiu@gmail.com>
2023-08-10 03:34:05 +0300

    Add function for download resume in file.pdf

--- builder/urls.py
+++ builder/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('create_resume/', views.create_resume, name= 'create_resume'),
+    path('generate_pdf/', views.generate_pdf, name='generate_pdf'),
 ]
--- builder/views.py
+++ builder/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponse, FileResponse
+from reportlab.pdfgen import canvas
 from django.shortcuts import render, redirect
 from .forms import ResumeForm
-
+import io
 
 def index(request):
 	title = "Main Page"
@@ -36,4 +38,46 @@ def create_resume(request):
 	
 	context = {'form': form}
 	return render(request, 'builder/create_resume.html', context)
+
+
+def generate_pdf(request):
+	if request.method == 'POST':
+		form = ResumeForm(request.POST)
+		if form.is_valid():
+			data = form.cleaned_data
+			
+			buffer = io.BytesIO()
+			p = canvas.Canvas(buffer)
+			
+			y_coordinate = 750  # Начальная координата Y для текста
+			
+			p.drawString(100, y_coordinate, f"Name: {data['first_name']} {data['last_name']}")
+			y_coordinate -= 20
+			
+			p.drawString(100, y_coordinate, f"Number: {data['phone_number']}")
+			y_coordinate -= 20
+			
+			p.drawString(100, y_coordinate, f"Email: {data['email']}")
+			y_coordinate -= 20
+			
+			p.drawString(100, y_coordinate, f"City: {data['city']}")
+			y_coordinate -= 20
+			
+			p.drawString(100, y_coordinate, f"Date of birth: {data['date_of_birth']}")
+			y_coordinate -= 20
+			
+			p.showPage()
+			p.save()
+			
+			buffer.seek(0)
+			response = FileResponse(buffer, content_type = 'application/pdf')
+			response['Content-Disposition'] = f'attachment; filename="{data["first_name"]}_{data["last_name"]}_resume.pdf"'
+			
+			return response
+	else:
+		form = ResumeForm()
+		
+		context = {'form': form}
+		return render(request, 'builder/resume_form.html', context)
+			
 		
