d0d0628caae6dc7f3843c7d716305aed06cd18a0
Kriukovchankiu <kryukovchanskiu@gmail.com>
2023-08-16 02:13:58 +0300

    Add 'add_password' function to encrypt PDF files

--- helper/views.py
+++ helper/views.py
@@ -1,8 +1,31 @@
+import PyPDF2
+from django.http import HttpResponse
 from django.shortcuts import render
+from io import BytesIO
 
 
 def add_password(request):
 	title = "Add password"
+	if request.method == 'POST':
+	
+		pdf_file = request.FILES['pdf_file']
+		password = request.POST['password']
+		
+		pdf_in = PyPDF2.PdfReader(pdf_file)
+		pdf_file = PyPDF2.PdfWriter()
+		
+		for page in pdf_in.pages:
+			pdf_file.add_page(page)
+		
+		pdf_file.encrypt(password)
+		
+		pdf_output = BytesIO()
+		pdf_file.write(pdf_output)
+		# with open('encrypted.pdf', 'wb') as f:
+		# 	pdf_file.write(f)
+		response = HttpResponse(pdf_output.getvalue(), content_type='application/pdf')
+		response['Content-Disposition'] = 'attachment; filename='"encrypted.pdf"
+		return response
 	
 	context = {
 		'title': title,
