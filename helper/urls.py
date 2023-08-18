549612cadcd4f10ff25eff870b0e68f95735420d
Kriukovchankiu <kryukovchanskiu@gmail.com>
2023-08-18 22:04:17 +0300

    Moved to another project

--- helper/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('add_password', views.add_password, name= 'add_password'),
-	path('delete_metadata', views.delete_metadata, name= 'delete_metadata'),
-	path('add_signature', views.add_signature, name= 'add_signature'),
-	path('optimize_file', views.optimize_file, name= 'optimize_file'),
-]
